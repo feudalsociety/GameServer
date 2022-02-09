7fdc0baf55866e74a80ea5dbc562cd1e806eed55
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2022-02-09 22:56:28 +0900

    deadlock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,6 +4,55 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    // 꼼수 - lock을 class로 wrapping한다.
+    class FastLock
+    {
+        public int id; // id 호출 순서를 추적했다가 graph 상에 cycle 있는지 판별
+    }
+
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            // Monitor.TryEnter()
+            lock(_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
@@ -11,53 +60,18 @@ namespace ServerCore
 
         static void Thread_1()
         {
-            for(int i = 0; i < 100000; i++)
+            for(int i = 0; i < 10000; i++)
             {
-                // 상호배제 Mutual exclusive
-                lock(_obj) // 알아서 열어줌
-                {
-                    number++;
-                }
-
-                // Critical section, c++ std::mutex
-                Monitor.Enter(_obj); // 문을 잠근다.
-                number++;
-                return;
-
-                // if(number == 10000)
-                // {
-                //     Monitor.Exit(_obj); 
-                //     return;
-                // }
-
-                Monitor.Exit(_obj);
-
-                // try
-                // {
-                //     Monitor.Enter(_obj);
-                //     number++;
-                //     return;
-                // }
-                // finally
-                // {
-                //     Monitor.Exit(_obj);
-                // }
+                SessionManager.Test();
             }
         }
 
         // Deadlock
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
-
-                // Monitor.Enter(_obj);
-                // number--;
-                // Monitor.Exit(_obj);
+                UserManager.Test();
             }
         }
 
@@ -66,6 +80,9 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(100);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
