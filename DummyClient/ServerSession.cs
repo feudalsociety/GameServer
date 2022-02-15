9ae7502a4f21cf65e6fa511ba0a185a48feb471a
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2022-02-15 22:17:02 +0900

    serialization (2)

--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -8,13 +8,12 @@ using System.Threading;
 
 namespace DummyClient
 {
-    // client쪽에 server 대리자가 ServerSession이고, 반대로 server에서 client의 대리인의 역할을 하는게 clientSession
-
-    // 헤더
-    class Packet
+    public abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
     }
 
     // client에서 요청
@@ -22,13 +21,44 @@ namespace DummyClient
     {
         public long playerId;
 
-    }
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
 
-    // Server에서 client로의 답변
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int attack;
+        public override void Read(ArraySegment<byte> s)
+        {
+            ushort count = 0;
+            // ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            // ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            this.playerId = BitConverter.ToInt64(s.Array, s.Offset + count);
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
+            count += 2;
+            // 굳이 packetid를 사용해야할까? (ushort)PacktID.PlayerInfoReq 써도됨. size도 packet 자체에서는 이용하지 않는다.
+            // 서버에서는 client가 항상 거짓말을 하고 있다고하고 만들어야 안정성이 올라간다.
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+            // size에 count 값을 복사
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count); 
+
+            if (success == false) return null;
+
+            return SendBufferHelper.Close(count);
+        }
     }
 
     public enum PacketID
@@ -39,44 +69,19 @@ namespace DummyClient
 
     class ServerSession : Session
     {
+
         // 엔진과 컨텐츠 분리
         public override void OnConnected(EndPoint endPoint)
         {
             Console.WriteLine("OnConnected : {0}", endPoint);
 
-            PlayerInfoReq packet = new PlayerInfoReq() { size = 4, packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001};
+            // size는 시작에서는 모르고 마지막에서야 알수 있다.
+            PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001 };
 
             // for (int i = 0; i < 5; i++)
             {
-                ArraySegment<byte> s = SendBufferHelper.Open(4096);
-
-                // s에 집어넣지않고 new byte하고 있어서 좀 비효율적, 다양한 해결방법이 있음
-                // 유니티에서 모든 버전을 사용할 수 있는지 확인해야함
-                ushort count = 0;
-                bool success = true;
-
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
-                count += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.packetId);
-                count += 2;
-                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.playerId);
-                count += 8;
-
-                // Getbyte는 안정성 때문에, 근데 속도가 느림
-                byte[] size = BitConverter.GetBytes(packet.size); // 2
-                byte[] packetId = BitConverter.GetBytes(packet.packetId); // 2
-                byte[] playerId = BitConverter.GetBytes(packet.playerId); // 8
-
-                // 자동화하기 위해서
-
-                Array.Copy(size, 0, s.Array, s.Offset + count, 2);
-                count += 2;
-                Array.Copy(packetId, 0, s.Array, s.Offset + count, 2);
-                count += 2;
-                Array.Copy(playerId, 0, s.Array, s.Offset + count, 8);
-                count += 8;
-                ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
-                Send(sendBuff);
+                ArraySegment<byte> s = packet.Write();
+                if(s != null) Send(s);
             }
         }
 
@@ -85,7 +90,7 @@ namespace DummyClient
             Console.WriteLine($"OnDisconnected : {endPoint}");
         }
 
-        // 앞으로 패킷으로 보낼거임, 짤려서 왔으면 실행하면 안될것이다.
+        // 앞으로 패킷으로 보낼거임, 짤려서 왔으면 실행하면 안될 것이다.
         // TCP에서는 100byte를 보낸다고해서 무조건 100byte들 받는다는 보장이 없다.
         public override int OnRecv(ArraySegment<byte> buffer)
         {
--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -11,10 +11,12 @@ using ServerCore;
 // Server에 연결하는 대상이 꼭 Client만 있는게 아니다. 서버 끼리도 dataBase를 관리하는 db서버랑도 통신할 수 있다.
 namespace Server
 {
-    class Packet
+    public abstract class Packet
     {
         public ushort size;
         public ushort packetId;
+        public abstract ArraySegment<byte> Write();
+        public abstract void Read(ArraySegment<byte> s);
     }
 
     // client에서 요청
@@ -22,13 +24,44 @@ namespace Server
     {
         public long playerId;
 
-    }
+        public PlayerInfoReq()
+        {
+            this.packetId = (ushort)PacketID.PlayerInfoReq;
+        }
 
-    // Server에서 client로의 답변
-    class PlayerInfoOk : Packet
-    {
-        public int hp;
-        public int attack;
+        // Size로 4byte를 보내주면 패킷을 조립하는 부분에서 4byte만 집어서 buffer로 넘겨줄거였음
+        // [][][][][][][][][][][][] 12 bytes / 4byte 뒷부분이 memory상에 데이터가 없는건 아니다.
+        // 따라서 size가 넘는지 check 안하고 강제로 뒤에 있는 값을 추출하면 정상적으로 추출이 된다.
+        public override void Read(ArraySegment<byte> s)
+        {
+            ushort count = 0;
+            // ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
+            count += 2;
+            // ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
+            count += 2;
+            this.playerId = BitConverter.ToInt64(new ReadOnlySpan<byte>(s.Array, s.Offset + count, s.Count - count));
+            count += 8;
+        }
+
+        public override ArraySegment<byte> Write()
+        {
+            ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+            ushort count = 0;
+            bool success = true;
+
+            // success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.packetId);
+            count += 2;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), this.playerId);
+            count += 8;
+            success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+
+            if (success == false) return null;
+
+            return SendBufferHelper.Close(count);
+        }
     }
 
     public enum PacketID
@@ -44,16 +77,6 @@ namespace Server
         {
             Console.WriteLine("OnConnected : {0}", endPoint);
 
-            //Packet packet = new Packet() { size = 100, packetId = 10 };
-
-            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-            //byte[] buffer = BitConverter.GetBytes(packet.size);
-            //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
-            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer2.Length, buffer2.Length);
-            //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
-            //Send(sendBuff);
-
             Thread.Sleep(5000);
             Disconnect();
         }
@@ -65,8 +88,27 @@ namespace Server
 
         public override void OnRecvPacket(ArraySegment<byte> buffer)
         {
+            // 버퍼의 크기를 벗어났는지 지속적으로 체크해야함
+            ushort count = 0;
+            // size가 실제 packet과 일치하는지 안하는지는 판별하기 어렵다.
+            // 패킷 헤더에 있는 정보는 믿을 수 없고 참고만 해야한다.
+            // 잘못된 값이 입력되어도 감지해야한다.
             ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + 2);
+            count += 2;
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
+            count += 2;
+
+            switch((PacketID)id)
+            {
+                case PacketID.PlayerInfoReq:
+                    {
+                        PlayerInfoReq p = new PlayerInfoReq();
+                        p.Read(buffer);
+                        Console.WriteLine($"PlayerInfoReq : {p.playerId}");
+                    }
+                    break;
+            }
+             
             Console.WriteLine($"RecvPacketId : {id}, Size : {size}");
         }
 
